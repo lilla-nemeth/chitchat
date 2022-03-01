b41e0887098154db9aa7274b43e092c1205808b5
lilla-nemeth <naemly@protonmail.com>
2022-03-01 16:42:22 +0200

    RoomContext has created

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
-import './App.css';
-import Home from './components/Home';
-import Chat from './components/Chat';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Home from "./components/Home";
+import Chat from "./components/Chat";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RoomContextProvider from "./context/RoomContext";
 
 function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="chat" element={<Chat />} />
-				</Routes>
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <RoomContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="chat" element={<Chat />} />
+          </Routes>
+        </BrowserRouter>
+      </RoomContextProvider>
+    </>
+  );
 }
 
 export default App;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,65 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import ButtonComponent from "./generic/ButtonComponent";
 import RoomButton from "./generic/RoomButton";
-import {
-  Main,
-  Wrapper,
-  Form,
-  Logo,
-  Label,
-  RoomContainer,
-  RoomIcon,
-} from "../Style";
+import { RoomContext } from "../context/RoomContext";
+import { Main, Wrapper, Form, Logo, Label, RoomContainer } from "../Style";
 import TextInput from "./generic/TextInput";
-import { ReactComponent as ChillIcon1 } from "../assets/icons/chill01.svg";
-import { ReactComponent as DebateIcon1 } from "../assets/icons/debate01.svg";
-import { ReactComponent as DebateIcon2 } from "../assets/icons/debate02.svg";
-import { ReactComponent as GameIcon1 } from "../assets/icons/game01.svg";
-import { ReactComponent as SportIcon1 } from "../assets/icons/sport01.svg";
-import { ReactComponent as SportIcon2 } from "../assets/icons/sport02.svg";
-import { ReactComponent as SportIcon3 } from "../assets/icons/sport03.svg";
-import { ReactComponent as SportIcon4 } from "../assets/icons/sport04.svg";
-import { ReactComponent as SportIcon5 } from "../assets/icons/sport05.svg";
-import { ReactComponent as TechIcon1 } from "../assets/icons/tech01.svg";
-import { ReactComponent as TechIcon2 } from "../assets/icons/tech02.svg";
 
 let DEBUG = false;
-
-const chatRooms = [
-  {
-    id: "1",
-    name: "Chill",
-    icon: <ChillIcon1 />,
-  },
-  {
-    id: "2",
-    name: "Debate",
-    // icon: <DebateIcon1 />,
-    icon: <DebateIcon2 />,
-  },
-  {
-    id: "3",
-    name: "Tech",
-    // icon: <TechIcon1 />,
-    icon: <TechIcon2 />,
-  },
-  {
-    id: "4",
-    name: "Game",
-    icon: <GameIcon1 />,
-  },
-  {
-    id: "5",
-    name: "Sport",
-    icon: <SportIcon1 />,
-    // icon: <SportIcon2 />,
-    // icon: <SportIcon3 />,
-    // icon: <SportIcon4 />,
-    // icon: <SportIcon5 />,
-  },
-];
-
-if (DEBUG) console.log(chatRooms);
 
 const handleSubmit = (e) => {
   e.preventDefault();
@@ -67,6 +13,9 @@ const handleSubmit = (e) => {
 
 function Home() {
   const [username, setUsername] = useState("");
+  const { rooms, setRooms } = useContext(RoomContext);
+
+  if (DEBUG) console.log(rooms);
 
   return (
     <Main>
@@ -83,7 +32,7 @@ function Home() {
 
           <Label>Room</Label>
           <RoomContainer>
-            {chatRooms.map((room) => (
+            {rooms.map((room) => (
               <RoomButton
                 key={room.id}
                 roomName={room.name}
