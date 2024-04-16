ea5276a54b41c7b662f02db8ab8d926d0957c57e
Lilla Németh <naemly@protonmail.com>
2024-04-16 15:36:15 +0300

    adding test link to home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link';
+
 export default function Home() {
-	return <h1>Home Page</h1>;
+	return (
+		<>
+			<h1>Home Page</h1>
+			<Link href='/chat'>Chat</Link>
+		</>
+	);
 }
