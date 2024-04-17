af55a4126614c7c83b18151b84946cbd125cf8f3
Lilla Németh <naemly@protonmail.com>
2024-04-17 14:35:47 +0300

    changing Send- and CloseIcon to tsx

--- frontend/src/app/assets/icons/SendIcon.tsx
+++ frontend/src/app/assets/icons/SendIcon.tsx
@@ -1,17 +1,9 @@
-function SendIcon() {
-  return (
-    <svg
-      version='1.0'
-      xmlns='http://www.w3.org/2000/svg'
-      viewBox='0 0 512.000000 512.000000'
-      preserveAspectRatio='xMidYMid meet'
-    >
-      <g
-        transform='translate(0.000000,512.000000) scale(0.100000,-0.100000)'
-        stroke='none'
-      >
-        <path
-          d='M4920 5110 c-80 -25 -4839 -1935 -4861 -1951 -37 -26 -63 -97 -55
+const SendIcon = () => {
+	return (
+		<svg version='1.0' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512.000000 512.000000' preserveAspectRatio='xMidYMid meet'>
+			<g transform='translate(0.000000,512.000000) scale(0.100000,-0.100000)' stroke='none'>
+				<path
+					d='M4920 5110 c-80 -25 -4839 -1935 -4861 -1951 -37 -26 -63 -97 -55
           -144 15 -77 12 -76 1020 -564 522 -253 953 -464 958 -469 5 -5 216 -436 469
           -958 488 -1008 487 -1005 564 -1020 47 -8 118 18 144 55 26 36 1941 4823 1955
           4884 9 43 8 53 -12 91 -37 69 -109 99 -182 76z m-1720 -1695 l-1135 -1135
@@ -19,10 +11,10 @@ function SendIcon() {
           -510 -1125 -1134z m594 -981 c-416 -1039 -759 -1892 -763 -1896 -4 -5 -174
           337 -379 759 l-372 768 1132 1132 c623 623 1133 1131 1135 1129 1 -1 -337
           -852 -753 -1892z'
-        />
-      </g>
-    </svg>
-  );
-}
+				/>
+			</g>
+		</svg>
+	);
+};
 
 export default SendIcon;
