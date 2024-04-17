0e3a8149cdfeb027725ef9f6bbc2b673123aa156
Lilla Németh <naemly@protonmail.com>
2024-04-17 12:51:45 +0300

    removing unexisting primary props; temporarly commented out useNavigate import

--- frontend/src/app/(routes)/page.tsx
+++ frontend/src/app/(routes)/page.tsx
@@ -1,10 +1,67 @@
-import Link from 'next/link';
+import React, { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import FormButton from '../components/FormButton';
+import RoomButton from '../components/RoomButton';
+import TextInput from '../components/TextInput';
+import { Main, Wrapper, Form, Logo, Label, RoomContainer } from '../styles';
 
-export default function Home() {
+const Home = () => {
+	const rooms = useSelector((state: any) => state.roomReducer.rooms);
+	//   const navigate = useNavigate();
+
+	const [username, setUsername] = useState('');
+	const [selectedRoom, setSelectedRoom] = useState(rooms[0].id);
+
+	const disabled = !username;
+
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+
+		// navigate(`chat/${selectedRoom}/${username}`);
+	};
+
 	return (
-		<>
-			<h1>Home Page</h1>
-			<Link href='/chat'>Chat</Link>
-		</>
+		<Main homeMain={true} mainHeight={true}>
+			<Wrapper>
+				<Logo primary>ChitChat</Logo>
+				<Form homeForm={true} onSubmit={handleSubmit}>
+					<TextInput
+						homeLabel={true}
+						primary={true}
+						name={'Username'}
+						labelName={'Username'}
+						value={username}
+						placeholder={'Username'}
+						onChange={(e: any) => setUsername(e.target.value)}
+						required={true}
+						autoFocus={true}
+					/>
+					<Label homeLabel={true}>Rooms</Label>
+					<RoomContainer>
+						{rooms.map((room: any) => (
+							<RoomButton
+								key={room.id}
+								roomName={room.name}
+								roomIcon={room.icon}
+								type={'radio'}
+								name={'selectedRoom'}
+								htmlFor={room.name}
+								id={room.id}
+								value={room.id}
+								selected={room.id === selectedRoom}
+								defaultChecked={room.id === selectedRoom}
+								onChange={(e: any) => {
+									setSelectedRoom(e.target.value);
+								}}
+							></RoomButton>
+						))}
+					</RoomContainer>
+					{disabled ? <FormButton primary={true} name={'Join'} /> : <FormButton primary={false} name={'Join'} />}
+				</Form>
+			</Wrapper>
+		</Main>
 	);
-}
+};
+
+export default Home;
