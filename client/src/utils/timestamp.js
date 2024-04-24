6817111ead0284fdfc499556b269dc69f0f854e4
Lilla Németh <naemly@protonmail.com>
2024-04-24 13:56:33 +0300

    deleting previous client folder

--- client/src/utils/timestamp.js
+++
@@ -1,7 +0,0 @@
-import Sugar from 'sugar';
-
-// sugar date format
-export const createTimestamp = (formatString) => {
-  const formattedDate = Sugar.Date.format(new Date(), formatString);
-  return formattedDate;
-};
