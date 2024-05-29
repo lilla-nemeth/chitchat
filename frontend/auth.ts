b776f00804a8e98b6972c6169a7f699e2c458783
Lilla Németh <naemly@protonmail.com>
2024-05-29 16:23:39 +0300

    Adding Auth0 to auth.ts config

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
+import Auth0 from 'next-auth/providers/auth0';
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-	providers: [],
+export const { handlers, auth, signIn, signOut } = NextAuth({
+	providers: [Auth0],
 });
