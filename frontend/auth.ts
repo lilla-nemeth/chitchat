53179e91fda82d86b50c96dafd8afffb6284d4cd
Lilla Németh <naemly@protonmail.com>
2024-05-30 16:35:57 +0300

    adding Auth0Provider with environment variables

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,6 +1,11 @@
 import NextAuth from 'next-auth';
-import Auth0 from 'next-auth/providers/auth0';
+import Auth0Provider from 'next-auth/providers/auth0';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-	providers: [Auth0],
+	providers: [
+		Auth0Provider({
+			clientId: process.env.AUTH0_CLIENT_ID,
+			clientSecret: process.env.AUTH0_CLIENT_SECRET,
+		}),
+	],
 });
