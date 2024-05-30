14ab40bcadd7474be937650727fb20fe244eaa07
Lilla Németh <naemly@protonmail.com>
2024-05-30 16:59:18 +0300

    type definition of env. variables;

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -4,8 +4,8 @@ import Auth0Provider from 'next-auth/providers/auth0';
 export const { handlers, auth, signIn, signOut } = NextAuth({
 	providers: [
 		Auth0Provider({
-			clientId: process.env.AUTH0_CLIENT_ID,
-			clientSecret: process.env.AUTH0_CLIENT_SECRET,
+			clientId: process.env.AUTH0_CLIENT_ID as string,
+			clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
 		}),
 	],
 });
